5ec8a38be5bac6f797fc18a6e3a4675d440a3774
Park Changyong <tempus157@gmail.com>
2022-04-04 07:04:33 +0900

    Make UI

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<input placeholder="First" />
+<br />
+<input placeholder="Last" />
+<br />
+<input placeholder="Decrease" />
+<br />
+
+<select>
+  <option value="3">3rd</option>
+  <option value="4">4th</option>
+  <option value="5">5th</option>
+  <option value="6" selected>6th</option>
+  <option value="7">7th</option>
+  <option value="8">8th</option>
+  <option value="9">9th</option>
+  <option value="10">10th</option>
+</select>
+<button type="button">Calculate</button>
+
+<h3>Result</h3>
+<div />
