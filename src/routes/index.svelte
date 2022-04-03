7a5946e68d0f6c77b32015ee4072318595148f3b
Park Changyong <tempus157@gmail.com>
2022-04-04 07:55:21 +0900

    Set default value of count

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
   const gears = [3, 4, 5, 6, 7, 8, 9, 10] as const;
-  const defaultGear = 6;
 
   let first = 3;
   let last = 3;
   let decrease = 1;
+  let count = 6;
 </script>
 
 <div>
@@ -12,18 +12,20 @@
     <input type="range" min="0.48" max="6" step="0.01" bind:value={first} />
     <span>{first.toFixed(2)}</span>
   </div>
+
   <div>
     <input type="range" min="0.48" max="6" step="0.01" bind:value={last} />
     <span>{last.toFixed(2)}</span>
   </div>
+
   <div>
     <input type="range" min="0" max="5" step="0.01" bind:value={decrease} />
     <span>{decrease.toFixed(2)}</span>
   </div>
 
-  <select>
+  <select bind:value={count}>
     {#each gears as gear}
-      <option value={gear} selected={gear === defaultGear}>{gear} Speed</option>
+      <option value={gear}>{gear} Speed</option>
     {/each}
   </select>
   <button type="button">Calculate</button>
